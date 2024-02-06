a9bb239a3ca6ade53c32132a94239ddb690d76a5
Sourav928 <khandekarsourav92@gmail.com>
2024-02-07 01:06:25 +0530

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import "./App.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 // components
-import Header from './components/Header';
-import TodoForm from './components/TodoForm';
-import Todos from './components/Todos';
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 function App() {
   return (
